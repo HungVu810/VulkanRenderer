234ebbc32d02274172cd5931b84166048a0d2d22
HungVu810 <63895487+HungVu810@users.noreply.github.com>
2023-02-21 22:11:53 -0800

    - removed unnecessary member variables since we don't need to hold on to them which cost memory spaces. - changed some member functions into free functions - changed some functions to work with RVO - added getSwapChainImages()

--- Vulkan/Source/Include/VulkanApplication.h
+++ Vulkan/Source/Include/VulkanApplication.h
@@ -14,9 +14,15 @@
 #include <cstring>
 #include <array>
 #include <functional>
-#include <cstdint> // needed for uint32_t
-#include <limits> // needed for std::numeric_limits
-#include <algorithm> // needed for std::clamp
+#include <cstdint> // Needed for uint32_t
+#include <limits> // Needed for std::numeric_limits
+#include <algorithm> // Needed for std::clamp
+
+#ifdef NDEBUG
+	const bool enableValidationLayers = false;
+#else
+	const bool enableValidationLayers = true;
+#endif
 
 // Use (void) to silence unused warnings.
 #define assertm(exp, msg) assert(((void)msg, exp))
@@ -25,42 +31,13 @@ constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
 constexpr auto print = [](const auto& in) { std::cout << in << '\n'; };
 
-#ifdef NDEBUG
-	const bool enableValidationLayers = false;
-#else
-	const bool enableValidationLayers = true;
-#endif
-
-class Names // forward declare
+// Forward declare --------- START
+namespace
 {
-public:
-	Names() : names{}, proxy{} {};
-	Names(std::vector<std::string>&& inNames) : names{ inNames }, proxy{} {};
-	[[nodiscard]] auto getProxy() noexcept -> const std::vector<const char*>&
-	{
-		proxy.clear();
-		proxy.resize(names.size());
-		const auto populateProxy = [this](size_t i)
-		{
-			proxy[i] = names[i].data();
-		};
-		std::ranges::for_each(std::views::iota(0u, proxy.size()), populateProxy);
-		return proxy;
-	}
-
-public:
-	std::vector<std::string> names;
-
-private:
-	std::vector<const char*> proxy; // Used with the Vulkan API, which is just vector names's std::string casted to const char* const
-};
-
-struct SurfaceAttributes
-{
-	vk::SurfaceCapabilitiesKHR capabilities;
-	std::vector<vk::SurfaceFormatKHR> formats;
-	std::vector<vk::PresentModeKHR> presentModes;
-};
+	class Names;
+	struct SurfaceAttributes;
+}
+// Forward declare --------- END
 
 class VulkanApplication
 {
@@ -76,18 +53,10 @@ private:
 	auto initWindow() -> void;
 
 	auto initDispatcher() -> void;
-	// initInstance --------- START
-	auto initLayer() noexcept -> void;
-	auto initInstanceExtension() noexcept -> void;
 	auto initInstance() -> void;
-	// initInstance --------- END
 	auto initDebugMessenger() -> void;
 	auto initSurface() -> void;
-	// initPhysicalDevice --------- START
-	auto initDeviceExtension() noexcept -> void;
-	auto initQueueCreateInfos(const vk::PhysicalDevice& physicalDevice) noexcept -> void;
 	auto initPhysicalDevice() -> void;
-	// initPhysicalDevice --------- END
 	auto initDevice() -> void;
 	auto initQueue() -> void;
 	auto initSwapChain() -> void;
@@ -107,22 +76,13 @@ private:
 
 private:
 	GLFWwindow* window;
-	vk::ApplicationInfo applicationInfo;
-	Names layer, instanceExtension, deviceExtension;
-	vk::DebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo;
-	vk::InstanceCreateInfo instanceCreateInfo;
 	vk::Instance instance;
+	vk::DebugUtilsMessengerCreateInfoEXT debugMessengerCreateInfo; // needed ?
 	vk::DebugUtilsMessengerEXT debugMessenger;
-	// stores a queue family index, and the associated queues + their priority level
-	using QueueFamilyIndex = uint32_t;
-	using QueuesPriority = std::vector<float>;
-	using QueueFamily = std::pair<QueueFamilyIndex, QueuesPriority>;
-	std::vector<QueueFamily> queueFamilies;
-	std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos;
+	vk::SurfaceKHR surface;
 	vk::PhysicalDevice physicalDevice;
 	vk::Device device;
 	vk::Queue queue;
-	vk::SurfaceKHR surface;
 	vk::SwapchainKHR swapChain;
 };
 
--- Vulkan/Source/VulkanApplication.cpp
+++ Vulkan/Source/VulkanApplication.cpp
@@ -1,15 +1,169 @@
 #include "VulkanApplication.h"
 VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
 
+namespace
+{
+	// Class/Structs
+	auto getNamesProxy(const std::vector<std::string>& names) noexcept -> std::vector<const char*>
+	{
+		std::vector<const char*> proxy;
+		const auto toCStr = [](std::string_view str) { return str.data(); };
+		std::ranges::move(names | std::views::transform(toCStr), std::back_inserter(proxy));
+		return proxy;
+	}
+	class Names
+	{
+	public:
+		[[nodiscard]] Names() : names{}, proxy{} {};
+		[[nodiscard]] Names(const std::vector<std::string>& strings) noexcept : names{ strings }, proxy{ getNamesProxy(names) } {}
+		[[nodiscard]] Names(std::vector<std::string>&& strings) noexcept : names{ strings }, proxy{ getNamesProxy(names) } {}
+		[[nodiscard]] Names(const Names& inNames) noexcept : names{ inNames.names }, proxy{ getNamesProxy(names) } {}
+		[[nodiscard]] Names(const Names&& inNames) noexcept : names{ inNames.names }, proxy{ getNamesProxy(names) } {}
+		// Todo: deducing this C++ 23 for both this & const this
+		//[[nodiscard]] auto getProxy() noexcept -> const std::vector<const char*>&
+		//{
+		//	proxy.clear();
+		//	populateNamesProxy(names, proxy);
+		//	return proxy;
+		//}
+		// This function is deleted because to use it, we must either return a
+		// temporary proxy, which is UB because the local proxy is detroyed after
+		// the function returned and its data is moved/copied to variable calling
+		// this function, or we must accept an external proxy variable so that its
+		// lifetime will out live this function.
+		// Consider making Names non-const
+		// [[nodiscard]] auto getProxy() const noexcept -> const std::vector<const char*> = delete;
+		[[nodiscard]] auto getProxy() const noexcept -> const std::vector<const char*>&
+		{
+			return proxy;
+		}
+
+	public:
+		std::vector<std::string> names;
+
+	private:
+		std::vector<const char*> proxy; // Used with the Vulkan API, which is just vector names's std::string casted to const char* const
+	};
+	struct SurfaceAttributes
+	{
+		vk::SurfaceCapabilitiesKHR capabilities;
+		std::vector<vk::SurfaceFormatKHR> formats;
+		std::vector<vk::PresentModeKHR> presentModes;
+	};
+
+	// Helper functions
+	using QueueFamilyIndex = uint32_t;
+	using QueuesPriorities = std::vector<float>;
+	using QueueFamily = std::pair<QueueFamilyIndex, QueuesPriorities>;
+	[[nodiscard]] auto getLayer() noexcept -> Names
+	{
+		if (!enableValidationLayers) return Names{};
+		std::string validationLayerName = "VK_LAYER_KHRONOS_validation";
+		std::vector<vk::LayerProperties> layersProperties = vk::enumerateInstanceLayerProperties();
+		assertm(!layersProperties.empty(), "No supported layers can be found");
+		const auto getLayerName = [](const vk::LayerProperties& availLayer) { return std::string_view(availLayer.layerName); };
+		const auto layersName = layersProperties | std::views::transform(getLayerName);
+		bool isSupported = std::ranges::find(layersName, validationLayerName) != std::end(layersName);
+		assertm(isSupported, "Validation layers requested, but is not supported!");
+		std::vector<std::string> names { validationLayerName };
+		return Names{ names };
+	}
+	[[nodiscard]] auto getInstanceExtension() noexcept -> Names
+	{
+		Names instanceExtension{};
+		uint32_t extensionCount = 0;
+		const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+		assertm(glfwExtensions, "Vulkan's surface extensions for window creation can not be found");
+		std::vector<std::string> names(glfwExtensions, glfwExtensions + extensionCount);
+		if (enableValidationLayers)
+		{
+			names.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+		}
+		return Names{ names };
+	}
+	[[nodiscard]] auto getDeviceExtension() noexcept -> Names
+	{
+		return Names{ { VK_KHR_SWAPCHAIN_EXTENSION_NAME } };
+	}
+	[[nodiscard]] auto getSuitablePhysicalDevice(const vk::Instance& instance, const std::function<bool (const vk::PhysicalDevice&)>& isSuitable) -> vk::PhysicalDevice
+	{
+		std::vector<vk::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();
+		assertm(!physicalDevices.empty(), "No physical device can be found");
+		const auto physicalDeviceIter = std::ranges::find_if(physicalDevices, isSuitable);
+		assertm(physicalDeviceIter != physicalDevices.end(), "Can't find a suitable GPU");
+		return *physicalDeviceIter;
+	}
+	[[nodiscard]] auto getSuitableSurfaceFormat(const std::vector<vk::SurfaceFormatKHR>& surfaceFormats) -> vk::SurfaceFormatKHR
+	{
+		const auto isSuitable = [](const auto& surfaceFormat)
+		{
+			return surfaceFormat.format == vk::Format::eB8G8R8A8Srgb
+				&& surfaceFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
+		};
+		const auto surfaceFormatsIter = std::ranges::find_if(surfaceFormats, isSuitable);
+		if (surfaceFormatsIter == surfaceFormats.end()) return surfaceFormats.at(0);
+		else return *surfaceFormatsIter;
+	}
+	[[nodiscard]] auto getSuitablePresentMode(const std::vector<vk::PresentModeKHR>& presentModes) -> vk::PresentModeKHR
+	{
+		const auto isSuitable = [](const auto& presentMode)
+		{
+			return presentMode == vk::PresentModeKHR::eMailbox;
+		};
+		const auto presentModesIter = std::ranges::find_if(presentModes, isSuitable);
+		if (presentModesIter == presentModes.end()) return vk::PresentModeKHR::eFifo;
+		else return *presentModesIter;
+	}
+	[[nodiscard]] auto getSuitableSurfaceExtent(GLFWwindow* window, const vk::SurfaceCapabilitiesKHR& surfaceCapabilities) -> vk::Extent2D
+{
+	if (surfaceCapabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
+	{
+		return surfaceCapabilities.currentExtent;
+	}
+	else
+	{
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		VkExtent2D actualExtent = { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
+		actualExtent.width = std::clamp(actualExtent.width, surfaceCapabilities.minImageExtent.width, surfaceCapabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, surfaceCapabilities.minImageExtent.height, surfaceCapabilities.maxImageExtent.height);
+		return actualExtent;
+	}
+}
+	[[nodiscard]] auto getSurfaceAttributes(const vk::SurfaceKHR& surface, const vk::PhysicalDevice& physicalDevice) noexcept -> SurfaceAttributes
+	{
+		return SurfaceAttributes{
+			.capabilities = physicalDevice.getSurfaceCapabilitiesKHR(surface),
+			.formats      = physicalDevice.getSurfaceFormatsKHR(surface),
+			.presentModes = physicalDevice.getSurfacePresentModesKHR(surface)
+		};
+	}
+	[[nodiscard]] auto getQueueFamilies(const vk::PhysicalDevice& physicalDevice, const std::function<bool(QueueFamilyIndex i)>& isSuitable) noexcept -> std::vector<QueueFamily>
+	{
+		const auto queueFamiliesProperties = physicalDevice.getQueueFamilyProperties();
+		const auto queueFamiliesIndex = std::views::iota(0u, queueFamiliesProperties.size());
+		const auto toQueueFamily = [](QueueFamilyIndex i) {
+			return QueueFamily{ i, { 1.0f } }; // one queue of priority level 1.0f
+		};
+		std::vector<QueueFamily> queueFamilies{};
+		std::ranges::move(
+			queueFamiliesIndex
+			| std::views::filter(isSuitable)
+			| std::views::transform(toQueueFamily)
+			, std::back_inserter(queueFamilies)
+		);
+		return queueFamilies;
+	}
+	[[nodiscard]] auto getSwapChainImages(const vk::Device& device, const vk::SwapchainKHR& swapChain) -> std::vector<vk::Image>
+	{
+		const auto Images = device.getSwapchainImagesKHR(swapChain);
+		return std::vector<vk::Image>{};
+	}
+}
+
 VulkanApplication::VulkanApplication()
 	: window{ nullptr }
-	, applicationInfo{
-		"Hello Triangle"
-		, VK_MAKE_VERSION(1, 0, 0)
-		, "No Engine"
-		, VK_MAKE_VERSION(1, 0, 0)
-		, VK_API_VERSION_1_0 }
-	, layer{}, instanceExtension{}, deviceExtension{}
+	, instance{}
 	, debugMessengerCreateInfo{
 		{}
 		, vk::DebugUtilsMessageSeverityFlagBitsEXT::eError
@@ -19,15 +173,11 @@ VulkanApplication::VulkanApplication()
 			| vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance
 			| vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation
 		, debugCallback }
-	, instanceCreateInfo{}
-	, instance{}
 	, debugMessenger{}
-	, queueFamilies{}
-	, queueCreateInfos{}
+	, surface{}
 	, physicalDevice{}
 	, device{}
 	, queue{}
-	, surface{}
 	, swapChain{}
 {
 }
@@ -74,42 +224,15 @@ auto VulkanApplication::initDispatcher() -> void
 	PFN_vkGetInstanceProcAddr vkGetInstanceProcAddr = dynamicLoader.getProcAddress<PFN_vkGetInstanceProcAddr>("vkGetInstanceProcAddr");
     VULKAN_HPP_DEFAULT_DISPATCHER.init(vkGetInstanceProcAddr); // support instance independent EXT function pointers
 }
-// initInstance --------- START
-auto VulkanApplication::initLayer() noexcept -> void
-{
-	if (!enableValidationLayers) return;
-	std::string validationLayerName = "VK_LAYER_KHRONOS_validation";
-	std::vector<vk::LayerProperties> availableLayers = vk::enumerateInstanceLayerProperties();
-	assertm(!availableLayers.empty(), "No layer can be found");
-	const auto extractLayerName = [](const vk::LayerProperties& availLayer) { return std::string_view(availLayer.layerName); };
-	const auto availableLayersName = availableLayers | std::views::transform(extractLayerName);
-	bool isSupported = std::ranges::find(availableLayersName, validationLayerName) != std::end(availableLayersName);
-	assertm(isSupported, "Validation layers requested, but is not supported!");
-	layer.names.push_back(validationLayerName);
-}
-auto VulkanApplication::initInstanceExtension() noexcept -> void
-{
-	uint32_t extensionCount = 0;
-	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
-	assertm(glfwExtensions, "Vulkan's surface extensions for window creation can not be found");
-	instanceExtension = std::vector<std::string>(glfwExtensions, glfwExtensions + extensionCount);
-	if (enableValidationLayers)
-	{
-		instanceExtension.names.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
-	}
-}
 auto VulkanApplication::initInstance() -> void
 {
-	initLayer();
-	initInstanceExtension();
-	instanceCreateInfo.setPApplicationInfo(&applicationInfo);
-	instanceCreateInfo.setPEnabledLayerNames(layer.getProxy());
-	instanceCreateInfo.setPEnabledExtensionNames(instanceExtension.getProxy());
-	if (!enableValidationLayers) return;
-	instanceCreateInfo.setPNext(&debugMessengerCreateInfo); // extend instance create info with debug messenger
+	const Names layer{ getLayer() };
+	const Names instanceExtension{ getInstanceExtension() };
+	vk::ApplicationInfo applicationInfo{ "Hello Triangle" , VK_MAKE_VERSION(1, 0, 0) , "No Engine" , VK_MAKE_VERSION(1, 0, 0) , VK_API_VERSION_1_0 };
+	vk::InstanceCreateInfo instanceCreateInfo{ {}, &applicationInfo, layer.getProxy(), instanceExtension.getProxy() };
+	if (enableValidationLayers) instanceCreateInfo.setPNext(&debugMessengerCreateInfo); // extend create info with debug messenger
 	instance = vk::createInstance(instanceCreateInfo);
 }
-// initInstance --------- END
 auto VulkanApplication::initDebugMessenger() -> void
 {
 	if (!enableValidationLayers) return;
@@ -123,59 +246,10 @@ auto VulkanApplication::initSurface() -> void
 	assertm(result == VK_SUCCESS, "Failed to create window surface");
 	surface = surfaceTemp;
 }
-auto getSurfaceAttributes(const vk::SurfaceKHR& surface, const vk::PhysicalDevice& physicalDevice) noexcept -> SurfaceAttributes
-{
-	return SurfaceAttributes{
-		.capabilities = physicalDevice.getSurfaceCapabilitiesKHR(surface),
-		.formats      = physicalDevice.getSurfaceFormatsKHR(surface),
-		.presentModes = physicalDevice.getSurfacePresentModesKHR(surface)
-	};
-}
-// initPhysicalDevice --------- START
-auto VulkanApplication::initDeviceExtension() noexcept -> void
-{
-	deviceExtension.names.push_back(VK_KHR_SWAPCHAIN_EXTENSION_NAME);
-}
-[[nodiscard]] auto getSuitablePhysicalDevice(const vk::Instance& instance, const std::function<bool (const vk::PhysicalDevice&)>& isSuitable) -> vk::PhysicalDevice
-{
-	std::vector<vk::PhysicalDevice> physicalDevices = instance.enumeratePhysicalDevices();
-	assertm(!physicalDevices.empty(), "No physical device can be found");
-	const auto physicalDeviceIter = std::ranges::find_if(physicalDevices, isSuitable);
-	assertm(physicalDeviceIter != physicalDevices.end(), "Can't find a suitable GPU");
-	return *physicalDeviceIter;
-}
-auto VulkanApplication::initQueueCreateInfos(const vk::PhysicalDevice& physicalDevice) noexcept -> void
-{
-	const auto physicalDeviceQueueFamilies = physicalDevice.getQueueFamilyProperties();
-	const auto queueFamiliesIndex = std::views::iota(0u, physicalDeviceQueueFamilies.size());
-	const auto suitableIndex = [&](uint32_t i)
-	{
-		const auto isGraphical = physicalDeviceQueueFamilies[i].queueFlags & vk::QueueFlagBits::eGraphics;
-		const auto isSurfaceSupported = physicalDevice.getSurfaceSupportKHR(i, surface); // for presentation
-		return isGraphical && isSurfaceSupported;
-	};
-	const auto toQueueFamily = [](uint32_t i) { return QueueFamily{ i, { 1.0f } }; };
-	std::ranges::move(
-		queueFamiliesIndex
-		| std::views::filter(suitableIndex)
-		| std::views::transform(toQueueFamily)
-		, std::back_inserter(queueFamilies)
-	);
-	assertm(!queueFamilies.empty(), "No desired queue family index found");
-	const auto toDeviceQueueCreateInfo = [](const auto& queueFamily) {
-		const auto& [familyIndex, queues] = queueFamily;
-		return vk::DeviceQueueCreateInfo{ {}, familyIndex, queues };
-	};
-	std::ranges::move(
-		queueFamilies
-		| std::views::transform(toDeviceQueueCreateInfo)
-		, std::back_inserter(queueCreateInfos)
-	);
-}
 auto VulkanApplication::initPhysicalDevice() -> void
 {
-	initDeviceExtension();
-	const auto isSuitable = [this](const vk::PhysicalDevice& physicalDevice)
+	Names deviceExtension{ getDeviceExtension() };
+	const auto isSuitable = [&, this](const vk::PhysicalDevice& physicalDevice)
 	{
 		vk::PhysicalDeviceProperties properties{ physicalDevice.getProperties() };
 		vk::PhysicalDeviceFeatures features{ physicalDevice.getFeatures() };
@@ -191,12 +265,24 @@ auto VulkanApplication::initPhysicalDevice() -> void
 	};
 	physicalDevice = getSuitablePhysicalDevice(instance, isSuitable);
 }
-// initPhysicalDevice --------- END
 auto VulkanApplication::initDevice() -> void
 {
 	const vk::PhysicalDeviceFeatures physicalDeviceFeatures{};
-	initQueueCreateInfos(physicalDevice);
-	vk::DeviceCreateInfo deviceCreateInfo{ {}, queueCreateInfos, layer.getProxy(), deviceExtension.getProxy(), &physicalDeviceFeatures};
+	Names layer{ getLayer() };
+	Names deviceExtension{ getDeviceExtension() };
+	const auto isSuitable = [&](QueueFamilyIndex i)
+	{
+		const auto isGraphical = physicalDevice.getQueueFamilyProperties()[i].queueFlags & vk::QueueFlagBits::eGraphics;
+		const auto isSurfaceSupported = physicalDevice.getSurfaceSupportKHR(i, surface); // for presentation
+		return isGraphical && isSurfaceSupported;
+	};
+	const auto toQueueCreateInfo = [](const auto& queueFamily) {
+		const auto& [queueFamilyIndex, queuePriorities] = queueFamily;
+		return vk::DeviceQueueCreateInfo{ {}, queueFamilyIndex, queuePriorities };
+	};
+	std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos{};
+	std::ranges::move(getQueueFamilies(physicalDevice, isSuitable) | std::views::transform(toQueueCreateInfo), std::back_inserter(queueCreateInfos));
+	vk::DeviceCreateInfo deviceCreateInfo{ {}, queueCreateInfos, layer.getProxy(), deviceExtension.getProxy(), &physicalDeviceFeatures };
 	device = physicalDevice.createDevice(deviceCreateInfo);
 }
 auto VulkanApplication::initQueue() -> void
@@ -205,55 +291,23 @@ auto VulkanApplication::initQueue() -> void
 	int queueIndex = 0; // TODO: always pick the first one tentatively for now
 	queue = device.getQueue(QueueFamilyIndex, queueIndex);
 }
-// initSwapChain --------- START
-[[nodiscard]] auto findSuitableSurfaceFormat(const std::vector<vk::SurfaceFormatKHR>& surfaceFormats) -> vk::SurfaceFormatKHR
-{
-	const auto isSuitable = [](const auto& surfaceFormat)
-	{
-		return surfaceFormat.format == vk::Format::eB8G8R8A8Srgb
-			&& surfaceFormat.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
-	};
-	const auto surfaceFormatsIter = std::ranges::find_if(surfaceFormats, isSuitable);
-	if (surfaceFormatsIter == surfaceFormats.end()) return surfaceFormats.at(0);
-	else return *surfaceFormatsIter;
-}
-[[nodiscard]] auto findSuitablePresentMode(const std::vector<vk::PresentModeKHR>& presentModes) -> vk::PresentModeKHR
-{
-	const auto isSuitable = [](const auto& presentMode)
-	{
-		return presentMode == vk::PresentModeKHR::eMailbox;
-	};
-	const auto presentModesIter = std::ranges::find_if(presentModes, isSuitable);
-	if (presentModesIter == presentModes.end()) return vk::PresentModeKHR::eFifo;
-	else return *presentModesIter;
-}
-[[nodiscard]] auto findSuitableSurfaceExtent(GLFWwindow* window, const vk::SurfaceCapabilitiesKHR& surfaceCapabilities) -> vk::Extent2D
-{
-	if (surfaceCapabilities.currentExtent.width != std::numeric_limits<uint32_t>::max())
-	{
-		return surfaceCapabilities.currentExtent;
-	}
-	else
-	{
-		int width, height;
-		glfwGetFramebufferSize(window, &width, &height);
-		VkExtent2D actualExtent = { static_cast<uint32_t>(width), static_cast<uint32_t>(height) };
-		actualExtent.width = std::clamp(actualExtent.width, surfaceCapabilities.minImageExtent.width, surfaceCapabilities.maxImageExtent.width);
-		actualExtent.height = std::clamp(actualExtent.height, surfaceCapabilities.minImageExtent.height, surfaceCapabilities.maxImageExtent.height);
-		return actualExtent;
-	}
-}
 auto VulkanApplication::initSwapChain() -> void
 {
 	const auto surfaceAttributes = getSurfaceAttributes(surface, physicalDevice);
-	const auto surfaceFormat = findSuitableSurfaceFormat(surfaceAttributes.formats);
-	const auto presentMode = findSuitablePresentMode(surfaceAttributes.presentModes);
-	const auto surfaceExtent = findSuitableSurfaceExtent(window, surfaceAttributes.capabilities);
+	const auto surfaceFormat = getSuitableSurfaceFormat(surfaceAttributes.formats);
+	const auto presentMode = getSuitablePresentMode(surfaceAttributes.presentModes);
+	const auto surfaceExtent = getSuitableSurfaceExtent(window, surfaceAttributes.capabilities);
 	auto imageCount = surfaceAttributes.capabilities.minImageCount + 1;
 	if (surfaceAttributes.capabilities.maxImageCount != 0) // not infinite
 	{
 		imageCount = std::min(imageCount, surfaceAttributes.capabilities.maxImageCount);
 	}
+	const auto isSuitableQueueFamilyIndex = [&](QueueFamilyIndex i)
+	{
+		const auto isGraphical = physicalDevice.getQueueFamilyProperties()[i].queueFlags & vk::QueueFlagBits::eGraphics;
+		const auto isSurfaceSupported = physicalDevice.getSurfaceSupportKHR(i, surface); // for presentation
+		return isGraphical && isSurfaceSupported;
+	};
 	vk::SwapchainCreateInfoKHR createInfo{
 		{}
 		, surface
@@ -264,7 +318,7 @@ auto VulkanApplication::initSwapChain() -> void
 		, 1 // specifies the amount of image's layers (ie: more than 1 for 3D stereoscopic application)
 		, vk::ImageUsageFlagBits::eColorAttachment // render directly onto the framebuffers (no post-processing)
 		, vk::SharingMode::eExclusive // drawing and presentation are done with one physical device's queue
-		, queueFamilies.front().first
+		, getQueueFamilies(physicalDevice, isSuitableQueueFamilyIndex).front().first // get the first queue family index
 		, surfaceAttributes.capabilities.currentTransform
 		, vk::CompositeAlphaFlagBitsKHR::eOpaque
 		, presentMode
@@ -273,7 +327,6 @@ auto VulkanApplication::initSwapChain() -> void
 	};
 	swapChain = device.createSwapchainKHR(createInfo);
 }
-// initSwapChain --------- END
 auto VulkanApplication::initVulkan() -> void
 {
 	assertm(glfwVulkanSupported() == GLFW_TRUE, "Vulkan is not supported");
@@ -291,6 +344,7 @@ auto VulkanApplication::initVulkan() -> void
 
 auto VulkanApplication::mainLoop() -> void
 {
+	const auto images = getSwapChainImages(device, swapChain);
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
